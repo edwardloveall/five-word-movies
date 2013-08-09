0f6eaddb947246fa91a021a486207848fbe87dca
Edward Loveall <edward@edwardloveall.com>
2013-08-09 16:44:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiveWordMovies::Application.config.secret_token = '1e99ece3849afe633a9d3ff17d963439bc9c6ff02de1208a23232aec701ef922a1e8ccff83477b76012fbd372da3a75f4e0e8e3932063ccf5b97f181cc6ab58d'
